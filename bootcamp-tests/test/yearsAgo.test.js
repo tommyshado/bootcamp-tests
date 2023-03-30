3a2f8c7bd0e05a7f61a1797499b15f8836accba1
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-03-30 15:42:05 +0200

    second commit

--- bootcamp-tests/test/yearsAgo.test.js
+++ bootcamp-tests/test/yearsAgo.test.js
@@ -6,4 +6,8 @@ describe('yearsAgo', () => {
     it('returns a number', () => {
         assert.equal(yearsAgo(1976), 47);
     })
+
+    it('returns a number', () => {
+        assert.equal(yearsAgo(2000), 23);
+    })
 });
